11fba04d17c8f8bc114a00f9c7ad062fdf3b9a49
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-13 09:35:06 +0530

    generic method

---
+++ genericmethod.java
@@ -0,0 +1,12 @@
+package Generics_and_File_handling;
+
+import java.util.ArrayList;
+
+public class genericmethod {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Vivek Yadav");
+        String name = al.get(0);
+        System.out.println("GFG!");
+    }
+}
